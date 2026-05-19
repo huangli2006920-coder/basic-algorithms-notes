b807887298d60006a842f2ae3b425a3768f3dd06
huangli2006920-coder <lysander_123@outlook.com>
2026-05-19 09:18:04 +0800

    Update P1255max.cpp

--- P1255max.cpp
+++ P1255max.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-int bignum[5005][3000];
-int len[5005];
+int bignum[5005][3000]; /// 数组 
+int len[5005]; ///位数
 
 
 
@@ -13,7 +13,7 @@ int main(void) {
 	cin >> n;
 
 	if (n == 1) {
-		cout << 1 << endl;
+		cout << 1 << endl; 
 		return 0;
 	}
 
@@ -32,21 +32,21 @@ int main(void) {
 	for (int i = 3 ; i <= n ; i ++) {
 		int carry = 0;
 		for (int j = 0 ; j < max(len[i - 1], len[i - 2]); j++) {
-			int sum = bignum[i - 1][j] + bignum[i - 2][j] + carry;
-			bignum[i][j] = sum % 10;
-			carry = sum / 10;
+			int sum = bignum[i - 1][j] + bignum[i - 2][j] + carry; ///位数相加
+			bignum[i][j] = sum % 10; ///余数
+			carry = sum / 10;   ///是否进位
 
 		}
-		if (carry > 0) {
-			bignum[i][max(len[i - 1], len[i - 2])] = carry;
-			len[i] = max(len[i - 1], len[i - 2]) + 1;
+		if (carry > 0) { ///进位
+			bignum[i][max(len[i - 1], len[i - 2])] = carry; /// carry =1 ；最高位 =1
+			len[i] = max(len[i - 1], len[i - 2]) + 1; ///位数 为1 
 		} else {
-			len[i] = max(len[i - 1], len[i - 2]);
+			len[i] = max(len[i - 1], len[i - 2]); ///位数相同
 		}
 	}
 
-	for (int i = len[n] - 1; i >= 0; i--) {
-		cout << bignum[n][i];
+	for (int i = len[n] - 1; i >= 0; i--) { 
+		cout << bignum[n][i]; ///倒序输出
 	}
 
 	cout << endl;
